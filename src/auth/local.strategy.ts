d5b83d67dc823110b4ebdd1b3a600035f5b5a53c
AmanKabra1 <amankabra.it24@gmail.com>
2025-01-15 16:38:02 +0530

    commit

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,23 @@
+import { validate } from 'class-validator';
+import { UserService } from 'src/users/users.service';
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
+import { User } from 'src/users/user.entity';
+import { UnauthorizedException } from '@nestjs/common';
 
-export class LocalStrategy extends PassportStrategy(Strategy){}
+export class LocalStrategy extends PassportStrategy(Strategy){
+    constructor(private readonly userService:UserService){
+        super()
+    }
+
+    validate(username:string,password:string):User{
+        const user:User= this.userService.getUserByName(username)
+        if(user == undefined){
+            throw new UnauthorizedException()
+        }
+
+        if(user!= undefined && user.password == password){
+            return user
+        }
+    }
+}
